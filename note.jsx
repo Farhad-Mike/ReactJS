aa68b04b348d6083185f33fd04f92b0bda024d0d
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2021-01-02 07:54:59 +0400

    u

--- note.jsx
+++ note.jsx
@@ -264,3 +264,90 @@ class App extends React.Component {
         )
     }
 }
+
+
+// Передача функции через props
+class App extends React.Component {
+
+    state = {
+        users: [
+            { name: 'Farhad', age: 27, id: 1 },
+            { name: 'Ayten', age: 24, id: 2 },
+            { name: 'Liza', age: 0, id: 3 }
+        ]
+    }
+
+    addUser = (user) => {
+        user.id = Math.random();
+        let users = [...this.state.users, user];
+        this.setState({
+            users
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Users users={ this.state.users }/>
+                <AddUser addUser={ this.addUser } />
+            </div>
+        )
+    }
+}
+
+
+function Users(props) {
+    let users = props.users.map(user => {
+        return (
+            <div key={ user.id }>
+                <div>{ user.name }</div>
+                <div>{ user.age }</div>
+                <div>{ user.id }</div>
+                <br/>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            {users}
+        </div>
+    )
+}
+
+class AddUser extends React.Component {
+    state = {
+        name: null,
+        age: null,
+        hair: null
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addUser(this.state)
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={ this.handleSubmit } onChange={ this.handleChange }>
+                <label htmlFor="name">Name: </label>
+                <input type="text" id='name' /> <br/> <br/>
+
+                <label htmlFor="age">Age: </label>
+                <input type="text" id='age' /> <br/> <br/>
+
+                <label htmlFor="hair">Hair: </label>
+                <input type="text" id='hair' /> <br/> <br/>
+
+                <button>Submit!</button>
+            </form>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
 class App extends React.Component {
+
+    state = {
+        users: [
+            { name: 'Farhad', age: 27, id: 1 },
+            { name: 'Ayten', age: 24, id: 2 },
+            { name: 'Liza', age: 0, id: 3 }
+        ]
+    }
+
+    addUser = (user) => {
+        user.id = Math.random();
+        let users = [...this.state.users, user];
+        this.setState({
+            users: users
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Users users={ this.state.users }/>
+                <AddUser addUser={ this.addUser } />
+            </div>
+        )
+    }
+}
+
+
+function Users(props) {
+    let users = props.users.map(user => {
+        return (
+            <div key={ user.id }>
+                <div>{ user.name }</div>
+                <div>{ user.age }</div>
+                <div>{ user.id }</div>
+                <br/>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            {users}
+        </div>
+    )
+}
+
+class AddUser extends React.Component {
     state = {
         name: null,
         age: null,
-        hair: null
+        id: null
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.addUser(this.state)
     }
 
     handleChange = (e) => {
